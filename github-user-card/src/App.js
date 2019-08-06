e3c3e9414dd22c4f9627439c8e553e7d5ce6989e
namistarr <namistarr@icloud.com>
2019-08-06 13:07:18 -0700

    axios call and set data to state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: []
+    }
+  }
+
+componentDidMount() {
+  axios.get(`https://api.github.com/users/namistarr`)
+  .then(response => {
+    console.log(response.data)
+    this.setState({userData: [response.data]})
+  })
+  .catch(error => {
+    console.log('error', error)
+  });
+}
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
-  );
+  )
+}
 }
 
 export default App;
